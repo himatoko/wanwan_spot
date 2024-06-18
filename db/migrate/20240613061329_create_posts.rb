1a76f17dbce3af73c91706bcd1a50193f648b04f
himatoko <160095919+himatoko@users.noreply.github.com>
2024-06-18 03:38:07 +0000

    [Add]新規投稿画面

---
+++ db/migrate/20240613061329_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.integer :user_id
+      t.string :title
+      t.string :shop_name
+      t.text :body
+      t.string :place
+      t.timestamps
+    end
+  end
+end
