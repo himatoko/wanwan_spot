2bbcbc802a5077a637fdd12e2eed1cdb4f013e0f
himatoko <160095919+himatoko@users.noreply.github.com>
2024-06-19 09:50:27 +0000

    [Add]マイページ詳細・編集画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
   end
   
   private
